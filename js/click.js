b70bd5a2f759728ed92aa86146696c7e583b6373
vaibhavpathak1994 <vaibhav_pathak@excellencetechnologies.in>
2017-02-03 12:08:37 +0530

    changes in the js/script.js

--- js/click.js
+++ js/click.js
@@ -1,19 +1,25 @@
-$(document).ready(function(){
-    $(".mapp").click(function(){
-    var a=parseFloat( $(this).find("input:hidden").attr("value")); //alert(a);
-    var b= parseFloat($(this).find("input:hidden").attr("value1"));  // alert(b);
-    
-  initMap(a,b);
-    function initMap(c,d) {
-      var location = {lat:c, lng:d};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
-          center: location,
-        });
-        var marker = new google.maps.Marker({
-          position: location,
-          map: map,
-        });
+$(document).ready(function()
+{
+    $(".mapp").click(function()
+     {
+        var a=parseFloat( $(this).find("input:hidden").attr("value")); //alert(a);
+        var b= parseFloat($(this).find("input:hidden").attr("value1"));  // alert(b);  
+        initMap(a,b);
+      });
+
+    function initMap(c,d) 
+      {
+         var location = {lat:c, lng:d};
+         var map = new google.maps.Map(document.getElementById('map'),
+         {
+             zoom: 4,
+             center: location,
+          });
+        var marker = new google.maps.Marker(
+        {
+            position: location,
+            map: map,
+          });
       }
-       });
+      
 });
